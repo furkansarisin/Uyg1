dfe2a7b7cfdbef39e3cce7232677a9d4ff046365
Furkan Sarışın <Furkan Sarn@DESKTOP-DQN25QC>
2024-01-18 20:07:16 +0300

    Proje dosyası ekle.

---
+++ Uyg1/Kisi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uyg1
+{
+    class Kisi
+    {
+        public string adi;
+        public string soyadi;
+        public string tel;
+        public string adres;
+        public string bilgiyiFormatla()
+        {
+
+
+            string cikti = "Adi: " + adi + "Soyadi: " + soyadi + "Tel:" + tel + "Adres:" + adres;
+            return cikti;
+        }
+}
+}
+
---
+++ Uyg1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uyg1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AdresDefteri defterim = new AdresDefteri();
+            defterim.Ekle("Furkan", "Sarışın", "+0539 ", "Kayseri");
+            defterim.Ekle("Cagri", "Demir ", "0553", "Adana");
+            defterim.Ekle("Mustafa Furkan", "Turkmen", "0555 ", "Eskisehir");
+            defterim.Ekle("Mustafa", "Gumuskalem", "0531", "Istanbul");
+
+            int indis = defterim.Bul("Furkan", "Sarışın");
+            Console.WriteLine(indis);
+
+            string veri = defterim.veriUret();
+            Raporlama rNesne = new Raporlama();
+            rNesne.printReport(veri);
+
+
+            int sonuc = defterim.Sil("Furkan", "Sarışın");
+            if (sonuc == -1)
+                Console.WriteLine("Silinemedi");
+            else
+                Console.WriteLine(sonuc + ". Kişi Silindi");
+
+            veri = defterim.veriUret();
+            rNesne.printReport(veri);
+            //Burada yukarıdaki sil işleminin çalışıp çalışmadığını kontrol ediyoruz
+        }
+    }
+
+
+
+
+}
